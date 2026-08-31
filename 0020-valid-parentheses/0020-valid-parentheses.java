c62da92c3fd3f71b438e8276789e3e8aaee2f761
vignesh s <vignesh.s2028@gmail.com>
2026-08-31 07:07:31 +0530

    Time: 3 ms (86.14%), Space: 43.2 MB (72.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(')');
+            }
+            else if (ch == '[') {
+                stack.push(']');
+            }
+            else if (ch == '{') {
+                stack.push('}');
+            }
+            else {
+                if (stack.isEmpty() || stack.pop() != ch) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
